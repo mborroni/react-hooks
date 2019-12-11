8aa905fed55493426057d065de6f260008948897
mborroni <milagros.borroni@mercadolibre.com>
2019-12-11 19:39:03 -0300

    add simple counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      Escribí acá tu componente
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Sumar</button>
+      <button onClick={() => setCount(count - 1)}>Restar</button>
+      <button onClick={() => setCount(0)}>Reiniciar</button>
     </div>
   );
 }
